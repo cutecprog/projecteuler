e3580f9561e92496891c8ea16b048ac18615ef09
Andrea Sarah Grooms <cutecprog@gmail.com>
2012-06-21 08:13:46 -0500

    Started solution(), isPalindromic() is a test place holder

---
+++ e4.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int solution(int *, int *);
+char isPalindromic(int);
+
+main()
+{
+   
+   printf("%i\n",problem4(0));
+}
+
+int solution(int *i, int *j)
+{
+   return 0;
+}
+
+// Test version
+char isPalindromic(int n)
+{
+   if(n==840885) //915*919
+      return 1;
+   return 0;
+}
+
