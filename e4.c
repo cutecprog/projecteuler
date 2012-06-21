1cdf53a7a951c89ea8a360a73b4278b05f3d094c
Andrea Sarah Grooms <cutecprog@gmail.com>
2012-06-21 08:29:14 -0500

    solution() works and problem description added to top of file.

--- e4.c
+++ e4.c
@@ -1,19 +1,29 @@
+/*
+A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 99.
+
+Find the largest palindrome made from the product of two 3-digit numbers.
+*/
+
 #include <stdio.h>
 
-int solution(int *, int *);
+void solution(int *, int *);
 char isPalindromic(int);
 
 main()
 {
    int i,j;
-   printf("%i\n",solution(&i, &j));
+   solution(&i, &j);
+   printf("%i * %i = %i\n", i, j, i*j);
 }
 
-int solution(int *i, int *j)
+void solution(int *i, int *j)
 {
    for(*i=999; *i>900; --*i)
-      printf("%i\n",*i);
-   return 0;
+      for(*j=999; *j>900; --*j)
+      if(isPalindromic(*i * *j))
+         return; // *i * *j is the answer
+   *i=0;
+   *j=0;
 }
 
 // Test version
