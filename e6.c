d9d3674af2e938bc92216b01c8ce45a74ca68af1
Andrea Sarah Grooms <cutecprog@gmail.com>
2012-06-23 08:13:42 -0500

    Started Euler problem 6.

---
+++ e6.c
@@ -0,0 +1,28 @@
+/*
+The sum of the squares of the first ten natural numbers is,
+
+1^2 + 2^2 + ... + 10^2 = 385
+The square of the sum of the first ten natural numbers is,
+
+(1 + 2 + ... + 10)^2 = 55^2 = 3025
+Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025  385 = 2640.
+
+Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+*/
+
+#include <stdio.h>
+
+int solution(int);
+
+main()
+{
+   printf("%i\n", solution(20));
+}
+
+/* Brute force method */
+int solution(int n)
+{
+   return n;
+}
+
+
