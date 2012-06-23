035d9bb31502999f796a7d644af178b999eb2347
Andrea Sarah Grooms <cutecprog@gmail.com>
2012-06-23 08:57:57 -0500

    Finished e6 using math.

--- e6.c
+++ e6.c
@@ -16,13 +16,34 @@ int solution(int);
 
 main()
 {
-   printf("%i\n", solution(20));
+   printf("%i\n", solution(100));
 }
 
-/* Brute force method */
+/* Math method method 
+Where sigma(1,n,{i}) == n*(n+1)/2 is true for any value of n
+and x^2 == x*x is true for any arithmatic expression of x.
+
+sigma(1,n,{i})^2 - sigma(1,n,{i^2})
+
+== (n*(n+1)/2)^2 - n*(n+1)*(2*n+1)/6
+
+== n^2(n+1)^2/4 - n*(n+1)*(2*n+1)/6
+
+== n*(n+1)*( n*(n+1)/4 - (2*n+1)/6 )
+
+== n*(n+1)*( 6*n*(n+1)/24 - 4*(2n+1)/24 )
+
+== n*(n+1)*(6*n^2-2*n-4)/24
+
+== n*(n+1)*(3*n^2-n-2)/12
+
+== (3*n*n-n-2)*(n+1)*n/12
+
+So I just used that final arithmatic expression.
+*/
 int solution(int n)
 {
-   return n;
+   return (3*n*n-n-2)*(n+1)*n/12;
 }
 
 
