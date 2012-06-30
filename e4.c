57abe0eca18a3e96f85227ea23dd454fa15a3a04
Andrea Sarah Grooms <cutecprog@gmail.com>
2012-06-30 09:19:58 -0500

    I just discovered a logical bug that's been in my program since the beginning. It was only chance that I found the solution. Basically the program stops once it finds a palindrome but it should stop only when it finds the largest palindrome.

--- e4.c
+++ e4.c
@@ -7,20 +7,21 @@ Find the largest palindrome made from the product of two 3-digit numbers.
 #include <stdio.h>
 #include <stdlib.h>
 
-void solution(int *i, int *j, const int BASE, const int DIGITS);
-char isPalindromic(int n, const int BASE, const int DIGITS);
+void solution(int *i, int *j, const unsigned int BASE, const unsigned int DIGITS);
+char isPalindromic(int n, const unsigned int BASE, const unsigned int DIGITS);
 
 main()
 {
-   int i,j, input;
+   int i,j, b, d;
    //printf("Enter a number: ");
-   //scanf(" %i", &input);
-   //printf("Is palin: %i\n", isPalindromic(input, 16, 5));
+   scanf(" %i", &b);
+   scanf(" %i", &d);
+   printf("Is palin: %i\n", isPalindromic(906609, 10, 6));
    
-   solution(&i, &j, 10, 3);
+   solution(&i, &j, b, d);
    //i=0x111111;
    //printf("%i\n", isPalindromic(i,16));
-   //printf("%x * %x = %x\n", i, j, i*j);
+   printf("%i * %i = %i\n", i, j, i*j);
 }
 
 /* Limited version
@@ -34,27 +35,38 @@ if(BASE==10 && DIGITS==3) {
    lowbound = 99;
 }
 */
-void solution(int *r1, int *r2, const int BASE, const int DIGITS)
+void solution(int *r1, int *r2, const unsigned int BASE, const unsigned int DIGITS)
 {
+   // 0 is the greatest palindrome that is a product of 1 or 0 digit numbers
+   if(DIGITS < 2) {
+      *r1=0;
+      *r2=0;
+      return;
+   }
+   
    int i, lowbound=1, highbound;
    for(i=0; i<(DIGITS-1); i++)
       lowbound*=BASE;
    highbound = lowbound*BASE-1;
    lowbound--;
    printf("High: %i\nLow: %i\n", highbound, lowbound);
-   /*
-   for(*r1=highbound; *r1>lowbound; --*r1)
-      for(*r2=highbound; *r2>lowbound; --*r2)
-      if(isPalindromic(*r1 * *r2, BASE, DIGITS))
-         return; // *i * *j is the answer
-   *i=0;
-   *j=0; // Shouldn't get here.*/
+   
+   for(*r1=highbound; *r1>lowbound; --*r1) {
+      for(*r2=highbound; *r2>lowbound; --*r2) {
+         if(isPalindromic(*r1 * *r2, BASE, DIGITS*2))
+            return; // *i * *j is the answer
+         printf("%i %i\n", *r1, *r2);
+      }
+      scanf(" %i", &i);
+   }
+   *r1=-1;
+   *r2=-1; // Shouldn't get here.
 }
 
 /* Iterative, general version
 Determines whether or not the arguement is a palindrome given a base and number of digits.
 */
-char isPalindromic(int n, const int BASE, const int DIGITS)
+char isPalindromic(int n, const unsigned int BASE, const unsigned int DIGITS)
 {
    int i, j;
    char isPalin = 1; // Holds whether or not argument is a palindrome.
