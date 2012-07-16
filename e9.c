483bf565aaf4a6befc5b3c3eadb5dd397169b188
Andrea Sarah Grooms <cutecprog@gmail.com>
2012-07-16 08:41:44 -0500

    Copied down most of the information I found useful from the notes I made to create the algorithm.

--- e9.c
+++ e9.c
@@ -21,13 +21,39 @@ main()
 }
 
 /*
- * Not Complete 
+ * Version: 12.07.16
+ * Returns a*b*c where a^2 + b^2 = c^2 and a + b + c = X and a, b, c, X are 
+ * natural numbers.
+ * 
+ * Further assumptions:
+ * a =< b < c therefore (X / 3 + 1) <= c <= (X - 2)
+ *                                1 <= b <  c
+ *                                1 <= a <= b
+ * Example:
+ * X = 1000
+ * a = 332, b = 333, c = 334
+ * a = 331, b = 334, c = 334 (base case)
+ * a = 332, b = 332, c = 335
+ * a = 331, b = 333, c = 335
+ * a = 330, b = 334, c = 335
+ * a = 329, b = 335, c = 335 (base case)
+ * a = 331, b = 332, c = 336
+ *          . . .
+ * a = 205, b = 370, c = 425
+ * a = 204, b = 371, c = 425
+ * a = 203, b = 372, c = 425
+ * a = 202, b = 373, c = 425
+ * a = 201, b = 374, c = 425
+ * a = 200, b = 375, c = 425 (final base case)
+ *
+ * Note:
+ * a truncates while b rounds so that a + b + c = X and a <=b
  */
 int solution(int a, int b, int c, const int X)
 {
         printf("~ a = %i, b = %i, c = %i\n", a, b, c);
         if(c == b) 
-                return solution((X - c - 1) / 2, 
+                return solution((X - c - 1) / 2,
                                 (X - c - 1) / 2 + ((c%2) ? 0 : 1),
                                 c+1, X);
         if(a * a + b * b == c * c)
