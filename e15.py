04e8bc6058bbb947edd581860e37b0aa99d1fac4
Andrea Sarah Grooms <cutecprog@gmail.com>
2012-07-25 16:26:57 -0500

    e15.py: Wrote a factorial function really quick.

--- e15.py
+++ e15.py
@@ -1,3 +1,16 @@
 #!/usr/bin/python3
 
-print("Euler Problem 15\n", end="")
+def main():
+        print("Euler Problem 15\n", end="")
+        print(factorial(4))
+
+def factorial(x):
+        product = 1
+        while x != 0:
+                product = product * x
+                x = x - 1
+        return product
+
+if __name__=="__main__":
+        main()
+        
