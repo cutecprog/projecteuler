ff4105a0760a92d6818bef6aba8438822a6892ed
Andrea Sarah Grooms <cutecprog@gmail.com>
2012-07-25 16:22:17 -0500

    Starting experimentation with e15.py

--- e15.c
+++ e15.c
@@ -1,11 +1,10 @@
 /*
  * Starting in the top left corner of a 2x2 grid, there are 6 routes (without 
- * backtracking) to the bottom right corner. Note: routes cannot go up or left.6
+ * backtracking) to the bottom right corner. Note: routes cannot go up or left.
  *
  * http://projecteuler.net/project/images/p_015.gif
  *
  * How many routes are there through a 20x20 grid?
- * 
  */
 
 #include <stdio.h>
---
+++ e15.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+
+print("Euler Problem 15\n", end="")
