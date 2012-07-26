cbff29860814cfe1b5298e712e0a251beb670387
Andrea Sarah Grooms <cutecprog@gmail.com>
2012-07-26 14:41:13 -0500

    e15.c: Writing test for all functions in test_sweep().

--- e15.c
+++ e15.c
@@ -37,11 +37,43 @@ void test_sweep()
         int input;
         int output;
         int answer;
-        
+        int i;
         struct path tmp;
-        init(&tmp, 21);
-        destruct(&tmp);
-        printf("Size: %i\n",tmp.size);
         
+        // void init(struct path *self, unsigned int size);
+        input = 3;
+        init(&tmp, input);
+        for(i = 0; i < input; i++) {
+                output = tmp.data[i];
+                answer = i;
+                if(output != i) {
+                        printf("! init(&tmp, %i) made tmp.data[%i] %i but "
+                                                "expected %i\n", input, i, 
+                                                output, answer);
+                        break;
+                }
+        }
+        
+        // void destruct(struct path *self);
+        answer = 0;
+        destruct(&tmp);
+        output = (int)tmp.data;
+        if(output != answer)
+                printf("! destruct(&tmp) returned with tmp.data as %i but "
+                                                "expected %i\n", output, answer
+                                                );
+        answer = 0;
+        output = tmp.size;
+        if(output != answer)
+                printf("! destruct(&tmp) returned with tmp.size as %i but "
+                                                "expected %i\n", output, answer
+                                                );
+        
+        // bool final_postion(const struct path *self);
+        // bool valid(const struct path *self);
+        // bool all_contiguous(const struct path *self);
+        // bool index_contiguous_to_next(const struct path *self, unsigned int index);
+        // void print_path(const struct path *self);
+        // void move_index_up(struct path *self, unsigned int index)
 }
 
--- e15_path.c
+++ e15_path.c
@@ -2,30 +2,37 @@
 #include "e15_path.h"
 
 /*
- * Version: 12.07.26
- * Sets up path struct.
+ * Version: 12.07.26 (not complete)
+ * Allocates memory and sets up path struct in initial postion EG 111000 if
+ * size is 3.
  */
 void init(struct path *self, unsigned int size)
 {
-        self->size = size;
-        self->data = (unsigned int *) malloc(sizeof(unsigned int) * size);
-}
-
-/*
- * Version: 12.07.26
- * Frees data from heap.
- */
-void destruct(struct path *self)
-{
-        self->size = 0;
-        free(self->data);
+        /*self->size = size;
+        self->data = (unsigned int *) malloc(sizeof(unsigned int) * size);*/
 }
 
 /*
  * Version: 12.07.26 (not complete)
+ * Frees data from heap.
+ */
+void destruct(struct path *self)
+{
+        /*self->size = 0;
+        free(self->data);*/
+}
+
+/*
+ * Version: 12.07.26 (not complete)
+ * Returns true if path.data in final position EG 000111 if size is 3.
  */
 bool final_postion(const struct path *self)
 {
+        /*int i;
+        for(i = 0; i < self->size; i++)
+                if(self->data[i] != self->size / 2 + i) 
+                        return false;
+        return true;*/
 }
 
 /*
