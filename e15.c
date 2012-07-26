5d6b049c5146ec9ebcd4cfe07d48a50b730e31b1
Andrea Sarah Grooms <cutecprog@gmail.com>
2012-07-26 15:43:09 -0500

    e15.c: test_sweep(): Realized I needed a try-catch for init() test so I used setjmp.h. I think I'm just going to write a macro in test_macros.h.

--- e15.c
+++ e15.c
@@ -8,6 +8,7 @@
  */
 
 #include <stdio.h>
+#include <setjmp.h>
 #include "test_macros.h"
 #include "e15_path.h"
                                                                                 
@@ -31,6 +32,18 @@ int solution(const int X)
 
 /*
  * Version: 12.07.26 (not complete)
+ * Test (e15_path.c):
+ *      init()
+ *      destruct()
+ *      final_position()
+ *      valid()
+ *      all_contiguous()
+ *      index_contiguous_to_next()
+ *      print_path()
+ *      move_index_up()
+ *
+ * Test (e15.c):
+ *      solution()
  */
 void test_sweep()
 {
@@ -39,12 +52,23 @@ void test_sweep()
         int answer;
         int i;
         struct path tmp;
+        jmp_buf env;            // For try-catch
+        int setjmp_value;
+
         
         // void init(struct path *self, unsigned int size);
         input = 3;
         init(&tmp, input);
         for(i = 0; i < input; i++) {
-                output = tmp.data[i];
+                setjmp_value = setjmp(env);
+                if(setjmp_value == 0) {         // try
+                        output = tmp.data[i];
+                        longjmp(env, 42);
+                } else {                        // catch
+                        printf("! init(&tmp, %i) made tmp.data[%i] ? but "
+                                                "expected %i\n", input, i, 
+                                                answer);
+                }
                 answer = i;
                 if(output != i) {
                         printf("! init(&tmp, %i) made tmp.data[%i] %i but "
@@ -71,7 +95,7 @@ void test_sweep()
         
         // bool final_position(const struct path *self);
         answer = false;
-        METHOD_TEST(final_position, &tmp, output, answer);
+        //METHOD_TEST(final_position, &tmp, output, answer);
         
         // bool valid(const struct path *self);
         // bool all_contiguous(const struct path *self);
