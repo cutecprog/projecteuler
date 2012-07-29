b13e1e6ad46c790bbb68bdedca69805d3de444e6
Andrea Sarah Grooms <cutecprog@gmail.com>
2012-07-29 13:10:30 -0500

    e15.py: Converted to iterative instead of recursion since the recursive function used to much memory at value 8000 and I wanted 8000 :P.

--- e15.py
+++ e15.py
@@ -1,7 +1,5 @@
-def solution(x):
-        if x == 0:
-                return 1
-        return solution(x - 1) * (4 * (x-1) + 2) / x
-        
-for i in range(0, 21):
-        print "%i: %i" % (i, solution(i))
+x = 1;
+for i in range(0, 8000):
+        x = x * (4 * (x - 1) + 2) / x
+
+print x
