240ba43d454c655414e16eba6140e61150cfd1c8
Andrea Sarah Grooms <cutecprog@gmail.com>
2012-08-12 13:33:45 -0500

    Completed it but answer didn't work. I'll see where I messed up.

--- e17.py
+++ e17.py
@@ -10,5 +10,50 @@
 # British usage.
 
 length = 0
-length += len("one")
+
+length += len("one")    * 9
+length += len("two")    * 9
+length += len("three")  * 9
+length += len("four")   * 9
+length += len("five")   * 9
+length += len("six")    * 9
+length += len("seven")  * 9
+length += len("eight")  * 9
+length += len("nine")   * 9
+
+length += len("ten")
+length += len("eleven")
+length += len("twelve")
+length += len("thirteen")
+length += len("fourteen")
+length += len("fifteen")
+length += len("sixteen")
+length += len("seventeen")
+length += len("eighteen")
+length += len("nineteen")
+
+length += len("twenty")  * 10
+length += len("thirty")  * 10
+length += len("fourty")  * 10
+length += len("fifty")   * 10
+length += len("sixty")   * 10
+length += len("seventy") * 10
+length += len("eighty")  * 10
+length += len("ninety")  * 10
+
+
+length += len("hundred") * 900
+length += len("one")    * 100
+length += len("two")    * 100
+length += len("three")  * 100
+length += len("four")   * 100
+length += len("five")   * 100
+length += len("six")    * 100
+length += len("seven")  * 100
+length += len("eight")  * 100
+length += len("nine")   * 100
+
+length += len("and")    * 99 * 9
+length += len("onethousand")
+
 print length
