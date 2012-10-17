be0b72871ba28422bae898958fd56e398902b2ef
cutecprog <cutecprog@gmail.com>
2012-10-17 14:28:41 -0500

    e18.py: add atot() switch generate a triangle from a string. Probably needs to be refactored but works. Oh yeah now to write that test.

--- e18.py
+++ e18.py
@@ -1,5 +1,29 @@
+test1 = """1
+2 3
+4 5 6
+7 8 9 10"""
+
 def main():
         print gsum([[1],[2,3],[4,5,6]])
+        print atot(test1)
+
+def atot(a):
+        """Converts a space delimited string to a 2d array (triangle).
+
+        """
+        output = []
+        i = 0
+        j = 0
+        for n in re.split("\n| ", a):
+                if j == i:
+                        j = 0
+                        i += 1
+                        output.append([])
+                output[i-1].append(int(n))
+                j += 1
+        return output
+                        
+        
 
 def gsum(data):
         """Computes the greatest possible sum of one traversal of a triangle.
@@ -20,4 +44,5 @@ def gsum(data):
         return S
 
 if __name__=="__main__":
+        import re
         main()
