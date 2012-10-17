471c72d5d622231d0ffc96acf1756e1116499283
cutecprog <cutecprog@gmail.com>
2012-10-17 13:55:47 -0500

    Started e18.py and just copied down everything I wrote on the board.

---
+++ e18.py
@@ -0,0 +1,23 @@
+def main():
+        gsum([[1],[2,3],[4,5,6]])
+
+def gsum(data):
+        """Computes the greatest possible sum of one traversal of a triangle.
+
+        """
+        S = 0
+        col_num = len(data)
+        sum_right = 0
+        sum_left = 0
+        for i in (1, col_num):
+                for j in (i, col_num):
+                        sum_left += data[j][0]
+                        sum_right += data[j][j]
+                if sum_left > sum_right:
+                        S += data[i][0]
+                else:
+                        S += data[i][i]
+        return S
+
+if __name__=="__main__":
+        main()
